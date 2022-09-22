285922bb5b7778b5b39f55ed8997405fa0a5f9ac
makarandrtx <makarand.ratnakar11@gmail.com>
2022-09-22 17:15:13 +0530

    Token Reference in C++ file

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    string tokenAccessID = "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";
+    cout << tokenAccessID << endl;
+    return 0;
+}
